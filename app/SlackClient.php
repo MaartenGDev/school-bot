a4b33e19d4d95e0cd008e20c33d1a67a95aebbf9
joostlawerman <joost.lawerman@gmail.com>
2016-09-30 19:53:14 +0200

    Reworked slackclient

--- app/SlackClient.php
+++ app/SlackClient.php
@@ -2,11 +2,31 @@
 
 namespace app;
 
+use App\Lesson;
 
 class SlackClient
 {
     protected $days = [];
     protected $lessons = [];
+    protected $colors;
+
+    public function __construct() {
+         $this->colors = collect([
+            '#1abc9c',
+            '#2ecc71',
+            '#3498db',
+            '#9b59b6',
+            '#34495e',
+            '#16a085',
+            '#27ae60',
+            '#16a085',
+            '#2980b9',
+            '#8e44ad',
+            '#2c3e50',
+            '#f1c40f'
+        ]);
+
+    }
 
     /**
      * Parses the name and returns
@@ -21,7 +41,7 @@ class SlackClient
         $dayName = trim(strtolower($dayName));
 
         // setting default to all, in case the input does not exist in array
-        $day = 'All'; 
+        $day = 'All';
         $days = [
             'All' => ['all', '', 'week'],
             'Monday' => ['monday', 'sunday', 'saturday', 'maandag', 'zaterdag', 'zondag', 'mendei', 'moandei', 'moanje', 'sneon', 'snein'],
@@ -60,66 +80,57 @@ class SlackClient
 
     public function parse($week)
     {
-
-        $colors = [
-            '#1abc9c',
-            '#2ecc71',
-            '#3498db',
-            '#9b59b6',
-            '#34495e',
-            '#16a085',
-            '#27ae60',
-            '#16a085',
-            '#2980b9',
-            '#8e44ad',
-            '#2c3e50',
-            '#f1c40f'
-        ];
+        $lessons = collect(json_decode($week));
 
 
-        $lessons = json_decode($week);
+        $message = $lessons->groupBy(function ($lesson) {
+            return date('d-m', strtotime($lesson->start_date));
+        })->map(function ($lessons) {
+            return $lessons->groupBy('long_name')->map(function ($lessons) {
+                return $lessons->map(function ($lesson) {
+                    return new Lesson($lesson, $this->colors->shift());
+                });
+            })->flatten();
+        });
 
+        //     $day =  date('l d-m-Y', strtotime($lesson->start_date));
 
-        $message = array_map(function ($lesson) use ($colors) {
+        //     $pretext = null;
+        //     $title = null;
 
-            $day = date('l d-m-Y', strtotime($lesson->start_date));
+        //     if (!in_array($day, $this->days)) {
+        //         $pretext = $day;
+        //         $this->days[] = $day;
+        //     }
 
-            $pretext = null;
-            $title = null;
+        //     if (!in_array($lesson->long_name, $this->lessons)) {
+        //         $this->lessons[] = $lesson->long_name;
+        //     }
 
-            if (!in_array($day, $this->days)) {
-                $pretext = $day;
-                $this->days[] = $day;
-            }
+        //     $firstTeacher = $lesson->lecturers ? $lesson->lecturers[0] : '-';
+        //     $firstRoom = $lesson->locations ? $lesson->locations[0]->building : '-';
 
-            if (!in_array($lesson->long_name, $this->lessons)) {
-                $this->lessons[] = $lesson->long_name;
-            }
+        //     $title = $lesson->long_name . ' - ' . $firstTeacher . ' - ' . $firstRoom;
 
-            $firstTeacher = $lesson->lecturers ? $lesson->lecturers[0] : '-';
-            $firstRoom = $lesson->locations ? $lesson->locations[0]->building : '-';
+        //     $color = $colors[array_search($lesson->long_name, $this->lessons)];
 
-            $title = $lesson->long_name . ' - ' . $firstTeacher . ' - ' . $firstRoom;
+        //     $times = date('H:i', strtotime($lesson->start_date)) . ' ' . date('H:i', strtotime($lesson->end_date));
 
-            $color = $colors[array_search($lesson->long_name, $this->lessons)];
-
-            $times = date('H:i', strtotime($lesson->start_date)) . ' ' . date('H:i', strtotime($lesson->end_date));
-
-            return (object)[
-                'fallback' => 'Required plain-text summary of the attachment.',
-                'color' => $color,
-                'pretext' => $pretext,
-                'start_date' => $lesson->start_date,
-                'end_date' => $lesson->end_date,
-                'fields' => [
-                    (object)[
-                        'title' => $title,
-                        'value' => $times,
-                        'short' => false
-                    ]
-                ]
-            ];
-        }, $lessons);
+        //     return (object)[
+        //         'fallback' => 'Required plain-text summary of the attachment.',
+        //         'color' => $color,
+        //         'pretext' => $pretext,
+        //         'start_date' => $lesson->start_date,
+        //         'end_date' => $lesson->end_date,
+        //         'fields' => [
+        //             (object)[
+        //                 'title' => $title,
+        //                 'value' => $times,
+        //                 'short' => false
+        //             ]
+        //         ]
+        //     ];
+        // });
 
 
         return [
